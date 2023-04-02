3b93701095683848f4e1347e8e232ef58205b1d7
aondodawid <aondo1@gmail.com>
2023-04-02 23:07:08 +0200

    Refactoring logic of the code

--- custom-select.js
+++ custom-select.js
@@ -6,44 +6,41 @@ function createCustomSelect() {
   /* Look for any elements with the class "custom-select" for one or if later 
 added more select tooltip: */
 
-  const customSelects = document.getElementsByClassName("custom-select");
+  const customSelects = document.getElementsByClassName('custom-select');
 
   for (const customSelect of customSelects) {
-    const selElmnt = customSelect.getElementsByTagName("select")[0];
+    const selElmnt = customSelect.getElementsByTagName('select')[0];
 
     /* For each element, create a new DIV that will act as the selected item: */
-    const selectSelected = document.createElement("DIV");
-    selectSelected.setAttribute("class", "select-selected");
-    selectSelected.innerHTML =
-      selElmnt.options[selElmnt.selectedIndex].innerHTML;
+    const selectSelected = document.createElement('DIV');
+    selectSelected.setAttribute('class', 'select-selected');
+    selectSelected.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
     customSelect.appendChild(selectSelected);
 
     /* For each element, create a new DIV that will contain the option list: */
-    const optionList = document.createElement("DIV");
-    optionList.setAttribute("class", "select-items select-hide");
+    const optionList = document.createElement('DIV');
+    optionList.setAttribute('class', 'select-items select-hide');
 
     for (const el of selElmnt) {
       /* For each option in the original select element,
     create a new DIV that will act as an option item: */
-      const option = document.createElement("DIV");
+      const option = document.createElement('DIV');
       option.innerHTML = el.innerHTML;
 
-      option.addEventListener("click", function (e) {
+      option.addEventListener('click', function (e) {
         /* When an item is clicked, update the original select box,
         and the selected item: */
-        const select =
-          this.parentNode.parentNode.getElementsByTagName("select")[0];
+        const select = this.parentNode.parentNode.getElementsByTagName('select')[0];
         const selected = this.parentNode.previousSibling;
-        for (let el of select) {
-          if (el.innerHTML == this.innerHTML) {
+        for (const el of select) {
+          if (el.innerHTML === this.innerHTML) {
             select.selectedIndex = Array.from(select).indexOf(el);
             selected.innerHTML = this.innerHTML;
-            const sameAsSelected =
-              this.parentNode.getElementsByClassName("same-as-selected");
+            const sameAsSelected = this.parentNode.getElementsByClassName('same-as-selected');
             for (const el of sameAsSelected) {
-              el.removeAttribute("class");
+              el.removeAttribute('class');
             }
-            this.setAttribute("class", "same-as-selected");
+            this.setAttribute('class', 'same-as-selected');
             break;
           }
         }
@@ -52,13 +49,13 @@ added more select tooltip: */
       optionList.appendChild(option);
     }
     customSelect.appendChild(optionList);
-    selectSelected.addEventListener("click", function (e) {
+    selectSelected.addEventListener('click', function (e) {
       /* When the select box is clicked, close any other select boxes,
     and open/close the current select box: */
       e.stopPropagation();
       closeAllSelect(this);
-      this.nextSibling.classList.toggle("select-hide");
-      this.classList.toggle("select-arrow-active");
+      this.nextSibling.classList.toggle('select-hide');
+      this.classList.toggle('select-arrow-active');
     });
   }
 
@@ -66,23 +63,21 @@ added more select tooltip: */
     /* A function that will close all select boxes in the document,
 except the current select box: */
 
-    const selectItem = document.getElementsByClassName("select-items");
-    const selectSelected = document.getElementsByClassName("select-selected");
-    let arrNo = [];
+    const selectItem = document.getElementsByClassName('select-items');
+    const selectSelected = document.getElementsByClassName('select-selected');
+    const arrNo = [];
 
     for (let i = 0; i < selectSelected.length; i++) {
-      elmnt == selectSelected[i]
-        ? arrNo.push(i)
-        : selectSelected[i].classList.remove("select-arrow-active");
+      elmnt == selectSelected[i] ? arrNo.push(i) : selectSelected[i].classList.remove('select-arrow-active');
     }
     for (let i = 0; i < selectItem.length; i++) {
-      arrNo.indexOf(i) ? selectItem[i].classList.add("select-hide") : "";
+      arrNo.indexOf(i) ? selectItem[i].classList.add('select-hide') : '';
     }
   }
 
   /* If the user clicks anywhere outside the select box,
 then close all select boxes: */
-  document.addEventListener("click", closeAllSelect);
+  document.addEventListener('click', closeAllSelect);
 
   /**
    * Custom dropdown select end
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import createCustomSelect from "./custom-select";
+import createCustomSelect from './custom-select';
 
-const colorContainer = document.querySelectorAll(".color-container");
-const form = document.getElementById("new-color");
-const notification = document.getElementById("message");
+const colorContainer = document.querySelectorAll('.color-container');
+const form = document.getElementById('new-color');
+const notification = document.getElementById('message');
 let hiddenAfter5Sec;
 
 createCustomSelect();
@@ -10,18 +10,16 @@ getData();
 
 function coppyMessage(message, data) {
   clearTimeout(hiddenAfter5Sec);
-  notification.classList.remove("hidden");
+  notification.classList.remove('hidden');
   notification.textContent = `${message} ${data}`;
   hiddenAfter5Sec = setTimeout(() => {
-    notification.classList.add("hidden");
+    notification.classList.add('hidden');
   }, 5000);
 }
 
 function copyToClipboard() {
   navigator.clipboard.writeText(this.dataset.color);
-  this.dataset.color != ""
-    ? coppyMessage("You copied the hex code: ", this.dataset.color)
-    : "";
+  this.dataset.color != '' ? coppyMessage('You copied the hex code: ', this.dataset.color) : '';
 }
 
 function render(arrData) {
@@ -32,26 +30,21 @@ function render(arrData) {
         <div class="color-name">${arrData[index]}</div>
       `;
   });
-  document
-    .querySelectorAll(".color")
-    .forEach((color) => color.addEventListener("click", copyToClipboard));
+  document.querySelectorAll('.color').forEach((color) => color.addEventListener('click', copyToClipboard));
 }
 
 function getData() {
-  const colorInput = document.forms["new-color"].elements[0].value.slice(1);
-  const colorScheme =
-    document.forms["new-color"].elements[1].value.toLowerCase();
+  const colorInput = document.forms['new-color'].elements[0].value.slice(1);
+  const colorScheme = document.forms['new-color'].elements[1].value.toLowerCase();
 
-  fetch(
-    `https://www.thecolorapi.com/scheme?hex=${colorInput}&mode=${colorScheme}&count=5`
-  )
+  fetch(`https://www.thecolorapi.com/scheme?hex=${colorInput}&mode=${colorScheme}&count=5`)
     .then((res) => res.json())
     .then((data) => {
       render(data.colors.map((el) => el.hex.value));
     });
 }
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
   getData();
 });
